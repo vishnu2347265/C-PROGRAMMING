ab7525818a1276758866a05d905507c040861cbe
vishnu2347265 <141517436+vishnu2347265@users.noreply.github.com>
2023-10-10 10:36:19 +0530

    .

--- Pharmacy Management System - 2347265/Pharmacy_Management_System.c
+++ Pharmacy Management System - 2347265/Pharmacy_Management_System.c
@@ -275,7 +275,7 @@ void MedicineDetails(struct Medicine m[], int number) {
             case 2: {
                 // Products About to Expire
                 printf("\nProducts About to Expire:\n");
-                // Implement logic to find and display products about to expire here
+               
                 break;
             }
             case 3:
